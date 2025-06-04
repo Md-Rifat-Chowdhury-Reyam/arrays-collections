208095bfb87ebd5d18244d85f2cad9058bec9d31
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-06-04 11:33:15 +0600

    2D array another rule

--- src/arrays/multiDimension/AnotherWay2D.java
+++ src/arrays/multiDimension/AnotherWay2D.java
@@ -1,4 +1,39 @@
 package arrays.multiDimension;
 
+import java.util.Scanner;
+
 public class AnotherWay2D {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter row value");
+        int n = scanner.nextInt();
+        System.out.println("enter column value");
+        int m = scanner.nextInt();
+
+        int[][] array = new int[n][m];
+
+        int t = 0;
+        System.out.println("enter matrix : ");
+        for (int i = 0; i < n; i++) {
+
+            //all rows have the same column count
+            t = m;
+            array[i] = new int[m]; //input columns
+
+            for (int j = 0; j < m; j++) {
+                array[i][j] = scanner.nextInt();
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+
+            System.out.println();
+        }
+        scanner.close();
+    }
 }
